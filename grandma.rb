948f6740cd53e30444eda077361c51a059fc313d
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-22 20:25:03 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
 # Write a speak_to_grandma method.
-
+if speak_to_grandma
+  puts " HUH?! SPEAK UP, SONNY!"
+  elsif
+  SPEAK_TO_GRANDMA
+  puts "NO, NOT SINCE 1938!"
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
