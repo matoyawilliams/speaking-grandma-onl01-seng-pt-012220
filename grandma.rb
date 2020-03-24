55e22b91699a500589b32f64e6ce64de70c3ea7f
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-24 19:34:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase == "HUH?! SPEAK UP, SONNY!"
-    elsif phrase.upcase == "NO, NOT SINCE 1938!"
+  elsif phrase.downcase == "HUH?! SPEAK UP, SONNY!"
+  else phrase.upcase == "NO, NOT SINCE 1938!"
   end
   
 
