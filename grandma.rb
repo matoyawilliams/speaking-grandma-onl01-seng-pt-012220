928db256df72b449733b07bcf894d56664f375ba
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-24 19:22:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma(phrase)
    
     elsif phrase == phrase.downcase
   		return "HUH?! SPEAK UP, SONNY!"
+  		
   	elsif phase == phrase.upcase
       return "NO, NOT SINCE 1938!"
     else
