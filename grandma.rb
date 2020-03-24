3efdda5e807796b5a0b30d9d21c016e1328928c4
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-24 19:38:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@ def speak_to_grandma(phrase)
     elsif phrase.upcase
       return "NO, NOT SINCE 1938!"
     # NO, NOT SINCE 1938!
-    else
+    elsif phrase.upcase
   		return "NO, NOT SINCE 1938!"
     end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
