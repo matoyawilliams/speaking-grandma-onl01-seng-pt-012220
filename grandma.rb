cd581d59aa10ff75ff7648857fe0ea6979de3f83
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-24 19:23:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,2 @@
-# Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
-  	elsif phase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    else
-      phrase == phrase.downcase
-  	  return "HUH?! SPEAK UP, SONNY!"
-    end
-end
-
+def 
 
