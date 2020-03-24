d7f6b3ade74af48ff599c18dff7c09153c044b78
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-24 19:36:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma(phrase)
+def speak_to_grandma("I LOVE YOU GRANDMA ")
   if phrase == "I LOVE YOU GRANDMA"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.downcase == "HUH?! SPEAK UP, SONNY!"
